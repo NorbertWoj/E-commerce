f9f75b5485212d6b4bcfc79c538d4f2eb549bcfc
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-02-05 12:05:16 +0100

    Improve ProductListComponent to correctly extract category ID from route parameters

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports:[CommonModule],
+  imports: [CommonModule],
   providers: [ProductService],
   templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css'
@@ -14,15 +15,30 @@ import { CommonModule } from '@angular/common';
 export class ProductListComponent {
 
   products: Product[] = [];
+  currentCategoryId: number | undefined;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    // cneck if 'id' parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the 'id' param string, convert string to a number using the '+' symbol
+      this.currentCategoryId = +(this.route.snapshot.paramMap.get('id') ?? 0);
+    } else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
