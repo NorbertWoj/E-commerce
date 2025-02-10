549fc4e9315cf813afdf38bc189e057978dd17e9
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-02-10 13:23:55 +0100

    Implement search component to interact with search route

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductCategoryMenuComponent],
+  imports: [RouterOutlet, ProductCategoryMenuComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
