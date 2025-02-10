3ba1591d1d0f6b45ba81a63e538638897b4fd87a
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-02-10 09:46:31 +0100

    Integrate menu component to replace static navigation links

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, ProductCategoryMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/components/product-category-menu/product-category-menu.component.ts
+++ frontend/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductCategory } from '../../common/product-category';
 import { ProductService } from '../../services/product.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-category-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './product-category-menu.component.html',
   styleUrl: './product-category-menu.component.css'
 })
