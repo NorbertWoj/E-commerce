e66cde27cfc9fc3e0f0eb087f0ffde97b02c0dc0
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-02-13 17:09:19 +0100

    Fetch product details from ProductService in ProductDetailsComponent

--- frontend/src/app/components/product-details/product-details.component.ts
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -9,4 +12,25 @@ import { Component } from '@angular/core';
 })
 export class ProductDetailsComponent {
 
+  product: Product | undefined;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {}
+  
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+
+  handleProductDetails() {
+    // get the "id" param string, convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
 }
