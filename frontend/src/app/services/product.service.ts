8aa51d2dc7c51822d45f49cef845829dd9ae421f
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-02-25 11:38:26 +0100

    Add pagination support to ProductService

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -45,6 +45,16 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  searchProductListPaginate( thePage: number, 
+                          thePageSize: number, 
+                          theKeyword: number): Observable<GetResponseProducts> {
+    // need to build URL based on keyword, page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
