ea27a82dece132d4aa3349388a97f393360402dc
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-02-26 08:42:13 +0100

    Create CartStatusComponent with HTML template

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { RouterOutlet } from '@angular/router';
 import { SearchComponent } from './components/search/search.component';
+import { CartStatusComponent } from "./components/cart-status/cart-status.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductCategoryMenuComponent, SearchComponent],
+  imports: [RouterOutlet, ProductCategoryMenuComponent, SearchComponent, CartStatusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
