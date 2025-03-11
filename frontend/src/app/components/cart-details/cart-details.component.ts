b80a6897127c2db09701bbadcb1e2fa81f48f8b4
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-03-11 11:53:24 +0100

    Mdify CartDetailsComponent to retrieve cart items

--- frontend/src/app/components/cart-details/cart-details.component.ts
+++ frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
   templateUrl: './cart-details.component.html',
   styleUrl: './cart-details.component.css'
 })
-export class CartDetailsComponent {
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor (private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    // get a ahandle to the cart iterms
+    this.cartItems = this.cartService.cartItems;
+
+    // subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // suscribce to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+    // compute cart total price and quantity
+    this.cartService.computeCartTotals();
+
+  }
 
 }
