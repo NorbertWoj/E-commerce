68ebabf88dbe1e49cfa65c4fdec873300ca64b24
NorbertWoj <norbert.wojcik.it@gmail.com>
2025-03-12 15:55:05 +0100

    Use routerLink instead of href

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SearchComponent } from './components/search/search.component';
 import { CartStatusComponent } from "./components/cart-status/cart-status.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductCategoryMenuComponent, SearchComponent, CartStatusComponent],
+  imports: [RouterOutlet, ProductCategoryMenuComponent, SearchComponent, CartStatusComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
