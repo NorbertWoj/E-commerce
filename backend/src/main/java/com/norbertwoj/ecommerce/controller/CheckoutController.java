3a444b0efbc4f9c301b7a8fbf67fca1ebcb1ef9a
NorbertWoj <norbert.wojcik.it@gmail.com>
2026-01-03 13:35:31 +0100

    Update CheckoutController to expose /payment-intent endpoint

--- backend/src/main/java/com/norbertwoj/ecommerce/controller/CheckoutController.java
+++ backend/src/main/java/com/norbertwoj/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.norbertwoj.ecommerce.controller;
 
+import com.norbertwoj.ecommerce.dto.PaymentInfo;
 import com.norbertwoj.ecommerce.dto.Purchase;
 import com.norbertwoj.ecommerce.dto.PurchaseResponse;
 import com.norbertwoj.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +28,14 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
