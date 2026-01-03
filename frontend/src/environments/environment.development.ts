525444237c02456b0435ab0eea7bdc8b493bbf81
NorbertWoj <norbert.wojcik.it@gmail.com>
2026-01-03 20:31:48 +0100

    Initial Stripe setup and payment DTO

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  stripePublishableKey: "pk_test_51SlCsRRW56mNoem40kJ4xVNMfiHqBXYnbusYApSKOgh50wDiPuk9Em94JF0uJ0zPzwY7iJXuEeAKRoVk8JhbMjeB00r1EDGuib", 
 
   api: {
     baseUrl: 'https://localhost:8443'
