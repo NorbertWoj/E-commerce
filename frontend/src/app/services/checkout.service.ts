4eff4b106635794068224c9637a9584024abbdfa
NorbertWoj <norbert.wojcik.it@gmail.com>
2026-01-03 20:34:09 +0100

    Update CheckoutService to create a PaymentIntent

--- frontend/src/app/services/checkout.service.ts
+++ frontend/src/app/services/checkout.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Purchase } from '../common/purchase';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,15 @@ export class CheckoutService {
 
   private purchaseUrl = `${environment.api.baseUrl}/api/checkout/purchase`;
 
+  private paymentIntentUrl = `${environment.api.baseUrl}/api/checkout/payment-intent`;
+
   constructor(private httpClient: HttpClient) { }
 
   placeOrder(purchase: Purchase): Observable<any> {
     return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+  createPaymentIntent(PaymentInfo: PaymentInfo): Observable<any> {
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl, PaymentInfo);
   }
 }
